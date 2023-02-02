0c70caf35e4cd36f4db6572b7ccd2ff4166dd85e
MatveiTrepelets <99071437+MatveiTrepelets@users.noreply.github.com>
2023-02-02 23:57:40 +0200

    Add files via upload

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,71 +1,78 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import s from './ContactForm.module.css';
+import {
+  useAddContactMutation,
+  useGetContactsApiQuery,
+} from 'redux/contactsApi';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
+export default function ContactForm() {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [addContact] = useAddContactMutation();
+  const { data } = useGetContactsApiQuery();
+
+  const handleChange = e => {
+    const prop = e.currentTarget.name;
+    switch (prop) {
+      case 'name':
+        setName(e.currentTarget.value);
+        break;
+      case 'phone':
+        setPhone(e.currentTarget.value);
+        break;
+      default:
+        throw new Error('Error');
+    }
   };
 
-  handleChange = name => e => {
-    const { target } = e;
-
-    this.setState(() => ({
-      [name]: target.value,
-    }));
-  };
-
-  handleSubmit = e => {
+  const handleAddContact = async e => {
     e.preventDefault();
-
-    const { onSubmit } = this.props;
-    onSubmit(this.state);
-    this.resetForm();
+    if (
+      data.find(contact => contact.name.toLowerCase() === name.toLowerCase())
+    ) {
+      setName('');
+      setPhone('');
+      return alert(`Number: ${name} is already in phonebook`);
+    }
+    if (name && phone) {
+      await addContact({ name: name, phone: phone }).unwrap();
+      setName('');
+      setPhone('');
+    }
   };
 
-  resetForm = () => {
-    this.setState(() => ({
-      name: '',
-      number: '',
-    }));
-  };
+  return (
+    <form className={s.form} onSubmit={handleAddContact}>
+      <label>
+        Name
+        <input
+          className={s.inputName}
+          value={name}
+          onChange={handleChange}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </label>
+      <label>
+        Number
+        <input
+          className={s.inputNumber}
+          value={phone}
+          onChange={handleChange}
+          type="tel"
+          name="phone"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </label>
 
-  render() {
-    return (
-      <form className={s.form} onSubmit={this.handleSubmit}>
-        <label>
-          Name
-          <input
-            className={s.inputName}
-            value={this.state.name}
-            onChange={this.handleChange('name')}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </label>
-        <label>
-          Number
-          <input
-            className={s.inputNumber}
-            value={this.state.number}
-            onChange={this.handleChange('number')}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </label>
-
-        <button type="submit" className={s.buttonEditor}>
-          Add contact
-        </button>
-      </form>
-    );
-  }
+      <button type="submit" className={s.buttonEditor}>
+        Add contact
+      </button>
+    </form>
+  );
 }
-
-export default ContactForm;
